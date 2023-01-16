f2d15e26e061fec896cd1e7aabab1543d66b203a
Gilmar Basilio <gilmar.basilio@gmail.com>
2023-01-16 00:22:20 -0300

    config projects

---
+++ purchases/src/http/http.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '../database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { resolve } from 'node:path';
+import { TestResolver } from '../test/test.resolver';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [TestResolver],
+  exports: [],
+})
+export class HttpModule {}
---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,16 @@
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useUser } from "@auth0/nextjs-auth0/client";
+
+export default function Home() {
+  const { user } = useUser();
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <a href="/api/auth/logout">Sair</a>
+    </div>
+  );
+}
+
+export const getServerSideProps = withPageAuthRequired();
